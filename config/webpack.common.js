196d5cfc7d785a3c81443a0cdab01a896dafd8df
GiancarloAparicio <zairo2542@gmail.com>
2020-10-30 23:27:12 -0500

    FIX: Error corregido por fuente inexistente

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,81 +1,75 @@
-const paths = require('./paths')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const paths = require('./paths');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+	entry: [paths.src + '/index.js'],
 
-  entry: [paths.src + '/index.js'],
+	output: {
+		path: paths.build,
+		filename: '[name].bundle.js',
+		publicPath: '/',
+	},
 
-  output: {
-    path: paths.build,
-    filename: '[name].bundle.js',
-    publicPath: '/',
-  },
+	plugins: [
+		new CleanWebpackPlugin(),
 
- 
-  plugins: [
-   
-    new CleanWebpackPlugin(),
+		new CopyWebpackPlugin([
+			{
+				from: paths.static,
+				to: 'assets',
+				ignore: ['*.DS_Store'],
+			},
+		]),
 
-    new CopyWebpackPlugin([
-      {
-        from: paths.static,
-        to: 'assets',
-        ignore: ['*.DS_Store'],
-      },
-    ]),
+		new HtmlWebpackPlugin({
+			title: 'Webpack',
+			favicon: paths.static + '/favicon.png',
+			template: paths.src + '/index.html',
+			filename: 'index.html', // output file
+		}),
+	],
 
-  
-    new HtmlWebpackPlugin({
-      title: 'Webpack Boilerplate',
-      favicon: paths.static + '/favicon.png',
-      template: paths.src + '/template.html', // template file
-      filename: 'index.html', // output file
-    }),
-  ],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader', 'eslint-loader'],
+			},
 
- 
-  module: {
-    rules: [
-     
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
-      },
+			{
+				test: /\.(scss|css)$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: { sourceMap: true, importLoaders: 1 },
+					},
+					{ loader: 'postcss-loader', options: { sourceMap: true } },
+					{ loader: 'sass-loader', options: { sourceMap: true } },
+				],
+			},
 
-    
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          'style-loader',
-          { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
-          { loader: 'postcss-loader', options: { sourceMap: true } },
-          { loader: 'sass-loader', options: { sourceMap: true } },
-        ],
-      },
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg|webp|svg)$/i,
+				loader: 'file-loader',
+				options: {
+					name: '[path][name].[ext]',
+					context: 'src', // prevent display of src/ in filename
+				},
+			},
 
-    
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg|webp|svg)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-          context: 'src', // prevent display of src/ in filename
-        },
-      },
-
-  
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-          name: '[path][name].[ext]',
-          context: 'src', 
-        },
-      },
-    ],
-  },
-}
+			{
+				test: /\.(woff(2)?|eot|ttf|otf|)$/,
+				loader: 'url-loader',
+				options: {
+					limit: 8192,
+					name: '[path][name].[ext]',
+					context: 'src',
+				},
+			},
+		],
+	},
+};
